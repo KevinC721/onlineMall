9230a660dec39f1e8db0cbb0d4eb75af85a9a768
KevinC606 <953395721@qq.com>
2020-05-07 16:41:24 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
+
+// 根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api';
+// 设置超时
+axios.defaults.timeout = 8000;
+// 拦截器，用于同一处理请求响应
+axios.interceptors.response.use(function(response) {
+  let res = response.data;
+  // 0 - 成功时返回数据
+  if(res.status == 0) {
+    return res.data;
+  // 10 - 未成功时
+  }else if(res.status == 10) {
+    window.location.href = "/#/login";
+  }else {
+    alert(res.msg);
+  }
+})
+
+
+Vue.use(VueAxios ,axios)
 Vue.config.productionTip = false
 
 new Vue({
--- src/router.js
+++ src/router.js
@@ -1,74 +1,74 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './pages/home'
-import Index from './pages/index'
-import Product from './pages/product'
-import Detail from './pages/detail'
-import Cart from './pages/cart'
-import Order from './pages/order'
-import OrderConfirm from './pages/orderConfirm'
-import OrderList from './pages/orderList'
-import orderPay from './pages/orderPay'
-import AliPay from './pages/alipay'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./pages/home";
+import Index from "./pages/index";
+import Product from "./pages/product";
+import Detail from "./pages/detail";
+import Cart from "./pages/cart";
+import Order from "./pages/order";
+import OrderConfirm from "./pages/orderConfirm";
+import OrderList from "./pages/orderList";
+import orderPay from "./pages/orderPay";
+import AliPay from "./pages/alipay";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router ({
+export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home,
       redirect: Index,
       children: [
         {
-          path: 'index',
-          name: 'index',
-          component: Index
+          path: "index",
+          name: "index",
+          component: Index,
         },
         {
-          path: 'product/:id',
-          name: 'product',
-          component: Product
+          path: "product/:id",
+          name: "product",
+          component: Product,
         },
         {
-          path: 'detail',
-          name: 'detail',
-          component: Detail
-        }
-      ]
+          path: "detail",
+          name: "detail",
+          component: Detail,
+        },
+      ],
     },
     {
-      path: '/cart',
-      name: 'cart',
-      component: Cart
+      path: "/cart",
+      name: "cart",
+      component: Cart,
     },
     {
-      path: '/order',
-      name: 'order',
+      path: "/order",
+      name: "order",
       component: Order,
       children: [
         {
-          path: 'list',
-          name: 'list',
-          component: OrderList
+          path: "list",
+          name: "list",
+          component: OrderList,
         },
         {
-          path: 'confirm',
-          name: 'confirm',
-          component: OrderConfirm
+          path: "confirm",
+          name: "confirm",
+          component: OrderConfirm,
         },
         {
-          path: 'pay',
-          name: 'pay',
-          component: orderPay
+          path: "pay",
+          name: "pay",
+          component: orderPay,
         },
         {
-          path: 'alipay',
-          name: 'alipay',
-          component: AliPay
-        }
-      ]
+          path: "alipay",
+          name: "alipay",
+          component: AliPay,
+        },
+      ],
     },
-  ]
-})
+  ],
+});
